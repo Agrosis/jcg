356b022df1af5f5a833d890960886b2e5c0d101e
Muhtasim Ahsan <agro@jantox.com>
2016-06-15 00:25:51 -0500

    Set up a child project for Gen traits.

--- project/Build.scala
+++ project/Build.scala
@@ -7,13 +7,13 @@ import bintray.Keys._
 object Build extends Build {
 
   val customBintraySettings = bintrayPublishSettings ++ Seq(
-    packageLabels in bintray       := Seq("json"),
+    packageLabels in bintray       := Seq("json", "codegen"),
     bintrayOrganization in bintray := Some("plasmaconduit"),
     repository in bintray          := "releases"
   )
 
   val root = Project("root", file("."))
-    .settings(customBintraySettings: _*)
+    //.settings(customBintraySettings: _*)
     .settings(
       name                  := "json-codegen",
       organization          := "com.plasmaconduit",
@@ -30,6 +30,21 @@ object Build extends Build {
       libraryDependencies   += "com.plasmaconduit" %% "json" % "0.21.0",
       libraryDependencies   += "com.eed3si9n" %% "treehugger" % "0.4.1",
       libraryDependencies   += "org.scala-lang" % "scala-reflect" % "2.11.8",
+      libraryDependencies   += "org.specs2" %% "specs2" % "2.3.11" % "test"
+    )
+
+  val traits = Project("traits", file("./traits"))
+    .settings(customBintraySettings: _*)
+    .settings(
+      name                  := "json-codegen-traits",
+      organization          := "com.plasmaconduit",
+      version               := "0.1.0",
+      crossScalaVersions    := Seq("2.10.4", "2.11.8"),
+      licenses              += ("MIT", url("http://opensource.org/licenses/MIT")),
+      scalacOptions         += "-feature",
+      scalacOptions         += "-deprecation",
+      scalacOptions         += "-unchecked",
+      scalacOptions in Test ++= Seq("-Yrangepos"),
       libraryDependencies   += "org.specs2" %% "specs2" % "2.3.11" % "test"
     )
 
